0d2b196a513460dc063ab83c6a17806925958c3f
MeftunH <maftunhashimli@gmail.com>
2021-07-29 04:04:40 +0400

    Initial Commit

---
+++ tests/Feature/meftunh/Press/Tests/InitialTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace meftunh\Press\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    public function firstTest()
+    {
+        $this->assertTrue(true);
+    }
+}
