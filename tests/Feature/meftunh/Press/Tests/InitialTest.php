eaec48efe00e4e26de8557fa0266ff4a8251ef31
MeftunH <maftunhasimli@gmail.com>
2021-08-05 00:09:13 +0400

    Initial Commit

--- tests/Feature/meftunh/Press/Tests/InitialTest.php
+++ tests/Feature/meftunh/Press/Tests/InitialTest.php
@@ -4,12 +4,12 @@
 namespace meftunh\Press\Tests;
 
 
-use Orchestra\Testbench\TestCase;
-
-class InitialTest extends TestCase
+class InitialTest
 {
-    public function firstTest()
+    public function parsedown_test()
     {
-        $this->assertTrue(true);
+//        $this->assertTrue(true);
+    $this->assertEquals('<h1>Heading</h1>', (new \League\CommonMark\Parser\MarkdownParser)->parse('# Heading'));
+
     }
 }
---
+++ tests/Unit/meftunh/Press/Tests/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace meftunh\Press\Tests;
+
+use MarkdownTest;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+
+}
